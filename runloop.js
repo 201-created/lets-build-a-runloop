6cf31a5bd931bd351051ee74b0c5990e8d313146
Matthew Beale <matt.beale@madhatted.com>
2014-08-17 13:55:54 -0400

    Introduce a runloop

--- runloop.js
+++ runloop.js
@@ -1,9 +1,30 @@
 (function(global){
 
+
   function Runloop(){
+    this.tasks = [];
   }
 
+  Runloop.currentRunloop;
 
+  Runloop.schedule = function(task){
+    if (!this.currentRunloop) {
+      throw "You cannot schedule a task without a runloop!";
+    }
+    this.currentRunloop.tasks.push(task);
+  }
+
+  Runloop.begin = function(){
+    this.currentRunloop = new Runloop();
+  }
+
+  Runloop.end = function(){
+    var task;
+    while (task = Runloop.currentRunloop.tasks.shift()) {
+      task();
+    }
+    this.currentRunloop = null;
+  }
 
   global.Runloop = Runloop;
 
