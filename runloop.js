20dd07911ddead8562be5bfa4ccf334759e53d94
Matthew Beale <matt.beale@madhatted.com>
2014-08-17 16:11:10 -0400

    Introduce multiple queues, scheduleOnce

--- runloop.js
+++ runloop.js
@@ -1,18 +1,59 @@
 (function(global){
   "use strict";
 
-
   function Runloop(){
-    this.tasks = [];
+    this.actions = [];
+    this.render = [];
   }
+
+  Runloop.prototype = {
+    scheduleOnce: function(queue, context, task) {
+      var foundTask;
+      for (var i=0;i<this[queue].length;i++) {
+        var queuedTask = this[queue][i];
+        if (queuedTask[0] === context && queuedTask[1] === task) {
+          foundTask = true;
+          break;
+        }
+      }
+      if (!foundTask) {
+        this[queue].push([context, task]);
+      }
+    },
+    schedule: function(queue, context, task) {
+      this[queue].push([context, task]);
+    },
+    flush: function(){
+      var task;
+      while (task = this.actions.shift()) {
+        task[1].call(task[0]);
+      }
+      while (task = this.render.shift()) {
+        task[1].call(task[0]);
+      }
+    }
+  };
 
   Runloop.currentRunloop = null;
 
-  Runloop.schedule = function(task){
+  Runloop.scheduleOnce = function(queue, context, task){
     if (!this.currentRunloop) {
       throw "You cannot schedule a task without a runloop!";
     }
-    this.currentRunloop.tasks.push(task);
+    this.currentRunloop.scheduleOnce(queue, context, task);
+  };
+
+  Runloop.schedule = function(queue, context, task){
+    if (!this.currentRunloop) {
+      throw "You cannot schedule a task without a runloop!";
+    }
+    this.currentRunloop.schedule(queue, context, task);
+  };
+
+  Runloop.run = function(context, fn){
+    this.begin();
+    fn.call(context);
+    this.end();
   };
 
   Runloop.begin = function(){
@@ -20,10 +61,7 @@
   };
 
   Runloop.end = function(){
-    var task;
-    while (task = Runloop.currentRunloop.tasks.shift()) {
-      task();
-    }
+    this.currentRunloop.flush();
     this.currentRunloop = null;
   };
 
