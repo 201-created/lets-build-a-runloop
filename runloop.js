d002b03967e6354f1393fd19ce9883c1bfdcf7fc
Cory Forsyth <cory.forsyth@gmail.com>
2014-08-27 00:47:43 +0200

    remove runloop innards

--- runloop.js
+++ runloop.js
@@ -8,25 +8,15 @@
   Runloop.currentRunloop = null;
 
   Runloop.schedule = function(context, task){
-    if (!this.currentRunloop) {
-      throw "You cannot schedule a task without a runloop!";
-    }
-    this.currentRunloop.tasks.push([context,task]);
+    throw "Implement Runloop.schedule";
   };
 
   Runloop.begin = function(){
-    this.currentRunloop = new Runloop();
+    throw "Implement Runloop.begin";
   };
 
   Runloop.end = function(){
-    var task;
-    while (task = Runloop.currentRunloop.tasks.shift()) {
-      var context = task[0];
-      task = task[1];
-
-      task.call(context);
-    }
-    this.currentRunloop = null;
+    throw "Implement Runloop.end";
   };
 
   global.Runloop = Runloop;
